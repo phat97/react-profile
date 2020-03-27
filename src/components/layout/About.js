0140bc23511c13c0a0a30042aed74bff2086e982
Phat <phat.le@hotmail.com>
2020-03-27 14:28:50 -0700

    About page finshed

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { FaBookReader } from 'react-icons/fa'
+import { GiBrain } from 'react-icons/gi'
 
 export const About = () => {
     return (
-        <div className="container" id="about">about</div>
+        <div className="container" id="about">
+            <div className="left-col">
+                <p className="about-title">Curious and creative, I am constantly pushing to accomplish my goal — to be more knowledgeable.</p>
+                <p className="about-subtitle">I satisfy my thirst for knowledge by exploring the complexity of web development and diving into the creative world of cooking.</p>
+            </div>
+            <div className="right-col">
+                <span className="book"><FaBookReader /></span>
+                <span className="brain"><GiBrain /></span>
+            </div>
+
+        </div>
     )
 }
