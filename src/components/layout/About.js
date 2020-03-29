947b38ffd6665e2e4853f55c41e053810cc6c076
Phat <phat.le@hotmail.com>
2020-03-28 18:33:24 -0700

    UI tweaks on about page

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -4,8 +4,8 @@ export const About = () => {
     return (
         <div className="container" id="about">
             <div className="left-col">
-                <p className="about-title">Curious and creative, I am constantly pushing myself to achieve my goals.</p>
-                <p className="about-subtitle">I satisfy my thirst for knowledge by exploring the complexity of web development and diving into the creative world of cooking.</p>
+                <p className="about-title">Curious and creative, I am constantly pushing for higher goals.</p>
+                <p className="about-subtitle">I enjoy exploring the complexity of web development and diving into the creative world of cooking.</p>
             </div>
             <div className="right-col">
                 <img className="me" src={process.env.PUBLIC_URL + "/images/me.jpg"} alt="me"></img>
@@ -16,9 +16,7 @@ export const About = () => {
                     <p>phat.le@hotmail.com</p>
                     <p>(778)-318-3441</p>
                 </div>
-
             </div>
-
         </div>
     )
 }
