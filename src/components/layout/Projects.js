a7133b847a9d3f67de8ac5f4c89967a283fc4105
Phat <phat.le@hotmail.com>
2020-03-30 18:08:00 -0700

    redesign carousel look

--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -9,18 +9,33 @@ export const Projects = () => {
 
     return (
         <div className="container" id="projects">
-            <h1 className="project-highlight" data-aos="fade-in" data-aos-duration="1000">Projects Highlight</h1>
-            <div className="project-container" data-aos="fade-up" >
+            {/* <h1 className="project-highlight" data-aos="fade-in" data-aos-duration="1000">Projects Highlight</h1> */}
+            <div className="project-container" data-aos="fade-up" data-aos-duration="1500" >
                 <CarouselProvider
                     naturalSlideWidth={100}
-                    naturalSlideHeight={38}
+                    naturalSlideHeight={45}
                     totalSlides={3}
                     infinite={true}
                 >
                     <Slider classNameAnimation="slider-animation">
-                        <Slide index={0}><div className="carosuel-item"><h1 className="slide-title">Weather App - <span className="secondary-text">Personal</span></h1></div></Slide>
-                        <Slide index={1}><div className="carosuel-item"><h1 className="slide-title">Tsunami Solution Billing Form - <span className="secondary-text">Professional</span></h1></div></Slide>
-                        <Slide index={2}><div className="carosuel-item"><h1 className="slide-title">TalkToVern - <span className="secondary-text">Professional</span></h1></div></Slide>
+                        <Slide index={0}>
+                            <div className="carosuel-item">
+                                <h1 className="slide-title">Weather App - <span className="secondary-text">Personal</span></h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis consectetur nisi ut pharetra. Aenean elit nulla, dignissim vitae urna in, ornare vulputate mi. Sed congue efficitur lectus, nec mattis mi mattis non. Vivamus est risus, dignissim sit amet nisl sed, laoreet tincidunt ipsum. Nam aliquet quis nisi vitae vehicula. Nunc quis sapien non eros convallis blandit non vitae diam. Aliquam volutpat mauris eu mi facilisis, eu dapibus arcu condimentum. Aliquam efficitur</p>
+                            </div>
+                        </Slide>
+                        <Slide index={1}>
+                            <div className="carosuel-item">
+                                <h1 className="slide-title">Tsunami Solution Billing Form - <span className="secondary-text">Professional</span>
+                                </h1>
+                            </div>
+                        </Slide>
+                        <Slide index={2}>
+                            <div className="carosuel-item">
+                                <h1 className="slide-title">TalkToVern - <span className="secondary-text">Professional</span>
+                                </h1>
+                            </div>
+                        </Slide>
                     </Slider>
                     <ButtonBack className="backward-btn"><FaChevronLeft className="slider-btn" /></ButtonBack>
                     <ButtonNext className="forward-btn"><FaChevronRight className="slider-btn" /></ButtonNext>
@@ -28,7 +43,6 @@ export const Projects = () => {
                         <DotGroup />
                     </div>
                 </CarouselProvider>
-
             </div>
         </div >
     )
