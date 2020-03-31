82edca6df13a61a21277241f87b602f08a4169be
Phat <phat.le@hotmail.com>
2020-03-30 17:16:09 -0700

    Added carousel

--- src/components/layout/Overview.js
+++ src/components/layout/Overview.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { FaChevronDown } from "react-icons/fa"
-import { Link } from 'react-scroll'
-import { animations } from '../../constants'
 
 export const Overview = () => {
     return (
@@ -11,9 +8,7 @@ export const Overview = () => {
                 <h1 className="name text-selection-none" data-aos="fade-up">Phat Le</h1>
                 <h1 className="position text-selection-none" data-aos="fade" data-aos-delay="800" data-aos-duration="1000" >WEB <span className="secondary-text" data-aos="fade" data-aos-duration="1500" data-aos-delay="1150">DEVELOPER</span></h1>
             </div>
-            <span className="arrow-down"><Link to="about" smooth={true} duration={animations.SCROLL_SPEED}><FaChevronDown /></Link></span>
             <div className="overlay"></div>
-
         </div >
     )
 }
--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 export const Projects = () => {
+
+
+
     return (
-        <div className="container" id="projects">projects</div>
+        <div className="container" id="projects">
+            <h1 className="project-highlight" data-aos="fade-in" data-aos-duration="1000">Projects Highlight</h1>
+            <div className="project-container" data-aos="fade-up" >
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={38}
+                    totalSlides={3}
+                    infinite={true}
+                >
+                    <Slider classNameAnimation="slider-animation">
+                        <Slide index={0}><div className="carosuel-item"><h1 className="slide-title">Weather App - <span className="secondary-text">Personal</span></h1></div></Slide>
+                        <Slide index={1}><div className="carosuel-item"><h1 className="slide-title">Tsunami Solution Billing Form - <span className="secondary-text">Professional</span></h1></div></Slide>
+                        <Slide index={2}><div className="carosuel-item"><h1 className="slide-title">TalkToVern - <span className="secondary-text">Professional</span></h1></div></Slide>
+                    </Slider>
+                    <ButtonBack className="backward-btn"><FaChevronLeft className="slider-btn" /></ButtonBack>
+                    <ButtonNext className="forward-btn"><FaChevronRight className="slider-btn" /></ButtonNext>
+                    <div className="dots">
+                        <DotGroup />
+                    </div>
+                </CarouselProvider>
+
+            </div>
+        </div >
     )
 }
