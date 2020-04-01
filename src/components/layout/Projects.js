e4b1c42f5b5fa624b1a9c30a180fa991336b9c19
Phat <phat.le@hotmail.com>
2020-03-31 17:00:47 -0700

    weather app content added

--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import { FiExternalLink } from 'react-icons/fi'
 
 export const Projects = () => {
 
@@ -16,12 +17,47 @@ export const Projects = () => {
                     naturalSlideHeight={45}
                     totalSlides={3}
                     infinite={true}
+                    dragEnabled={false}
                 >
                     <Slider classNameAnimation="slider-animation">
                         <Slide index={0}>
                             <div className="carosuel-item">
-                                <h1 className="slide-title">Weather App - <span className="secondary-text">Personal</span></h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis consectetur nisi ut pharetra. Aenean elit nulla, dignissim vitae urna in, ornare vulputate mi. Sed congue efficitur lectus, nec mattis mi mattis non. Vivamus est risus, dignissim sit amet nisl sed, laoreet tincidunt ipsum. Nam aliquet quis nisi vitae vehicula. Nunc quis sapien non eros convallis blandit non vitae diam. Aliquam volutpat mauris eu mi facilisis, eu dapibus arcu condimentum. Aliquam efficitur</p>
+                                <h1 className="slide-title">Today's Forecast - <span className="secondary-text">Personal</span></h1>
+                                <div className="carousel-grid">
+                                    <div className="project-detail">
+                                        <p className="project-header">Description:</p>
+                                        <p>Today's Forecast is a web application that takes in the user input of a city and return the current
+                                        temperature and weather condition of the location. </p>
+                                        <p>The application is built using C#, ASP.net Core 3.1 and RazorPages.
+                                        I used GooglePlaceAPI for the auto-complete search and OpenWeatherAPI to get the weather condition.</p>
+                                        <a href="https://pl-weather-app.herokuapp.com/"><p className="site-url">Visit The Site <FiExternalLink /></p></a>
+                                    </div>
+                                    <div className="project-tech">
+                                        <p className="project-header">Technologies Used:</p>
+                                        <table className="tech-table">
+                                            <tr>
+                                                <td>&#8226; C#</td>
+                                                <td>&#8226; ASP.Net Core 3.1</td>
+                                                <td>&#8226; RazorPages</td>
+                                                <td>&#8226; MVC</td>
+                                            </tr>
+                                            <tr>
+                                                <td>&#8226; JavaScript</td>
+                                                <td>&#8226; GooglePlaceAPI</td>
+                                                <td>&#8226; Heroku</td>
+                                            </tr>
+                                            <tr>
+                                                <td>&#8226; HTML/CSS</td>
+                                                <td>&#8226; OpenWeatherAPI</td>
+                                                <td>&#8226; Docker</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div className="project-example">
+                                        <img src={process.env.PUBLIC_URL + "/images/WeatherProject.jpg"} alt="Weather App"></img>
+                                    </div>
+
+                                </div>
                             </div>
                         </Slide>
                         <Slide index={1}>
