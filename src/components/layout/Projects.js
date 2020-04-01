332516b11d3674574e2ef7f5bd4f79aef3c06c76
Phat <phat.le@hotmail.com>
2020-04-01 01:30:09 -0700

    TalkToVern content added to project

--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -65,7 +65,7 @@ export const Projects = () => {
                                 <div className="carousel-grid">
                                     <div className="project-detail">
                                         <p className="project-header">Description:</p>
-                                        <p>SafetyLine billing form is a project selected as part of the BCIT curriculum called a practicum. It is
+                                        <p>SafetyLine billing form is a 1 month long project selected as part of the BCIT curriculum called a practicum. It is
                                         essentially an unpaid internship to gain work experience.
                                         </p>
                                         <p>My team and I converted an outdate .NET desktop application into a
@@ -103,6 +103,43 @@ export const Projects = () => {
                             <div className="carosuel-item">
                                 <h1 className="slide-title">TalkToVern - <span className="secondary-text">Professional</span>
                                 </h1>
+                                <div className="carousel-grid">
+                                    <div className="project-detail">
+                                        <p className="project-header">Description:</p>
+                                        <p>TalkToVern is a 4 month long project selected as part of the BCIT curriculum called a practicum.
+                                        It is essentially an unpaid internship to gain work experience.
+                                        </p>
+                                        <p>The task was to design and create a web application that plays audio clip to the user and record the user. The recordings
+                                        are then uploaded to the server and sent to an admin.
+                                        </p>
+                                        <p>My team and I spent the first month designing the aritechure of the system. I was responsible for working on the backend and
+                                        dealing with the intergration and testing. The project took over 700 man hours to complete by the end of the 4 month.
+                                        </p>
+                                    </div>
+                                    <div className="project-tech">
+                                        <p className="project-header">Technologies Used:</p>
+                                        <table className="tech-table">
+                                            <tr>
+                                                <td>&#8226; C#</td>
+                                                <td>&#8226; ASP.Net Core 3.0</td>
+                                                <td>&#8226; RazorPages</td>
+                                            </tr>
+                                            <tr>
+                                                <td>&#8226; JavaScript</td>
+                                                <td>&#8226; EntityFramework</td>
+                                                <td>&#8226; MVC</td>
+                                            </tr>
+                                            <tr>
+                                                <td>&#8226; HTML/CSS</td>
+                                                <td>&#8226; Microsoft Azure</td>
+                                                <td>&#8226; MySQL</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div className="project-example">
+                                        <img src={process.env.PUBLIC_URL + "/images/VERN Logo.jpg"} alt="TalkToVern Logo"></img>
+                                    </div>
+                                </div>
                             </div>
                         </Slide>
                     </Slider>
