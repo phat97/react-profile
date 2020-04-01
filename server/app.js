d54b264ebe1eae7865fad41da8ba87ce2af8a7e3
Phat <phat.le@hotmail.com>
2020-04-01 15:56:35 -0700

    Added node server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+const bodyParser = require('body-parser')
+const port = 8080;
+
+app.use(express.static('build'))
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../build", 'index.html'))
+});
+
+app.use("/", router);
+let listener = app.listen(process.env.PORT || port, () => {
+    console.log(`Listening on port ${listener.address().port}`);
+});
